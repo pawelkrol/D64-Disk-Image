94e29ff816f473a38ecf848c46a28a472393677f
Pawel Krol <djgruby@gmail.com>
2013-01-12 14:34:20 +0100

    Increased module version number to 0.02

--- lib/D64/Disk/Image.pm
+++ lib/D64/Disk/Image.pm
@@ -128,7 +128,7 @@ use constant T_REL => 4;
 use constant T_CBM => 5;
 use constant T_DIR => 6;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp qw/carp croak verbose/;
 
@@ -763,7 +763,7 @@ Pawel Krol, E<lt>pawelkrol@cpan.orgE<gt>.
 
 =head1 VERSION
 
-Version 0.01 (2010-10-10)
+Version 0.02 (2013-01-12)
 
 =head1 COPYRIGHT AND LICENSE
 
--- lib/D64/Disk/Image/File.pm
+++ lib/D64/Disk/Image/File.pm
@@ -67,7 +67,7 @@ $EXPORT_TAGS{'all'} = [ @{$EXPORT_TAGS{'modes'}} ];
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp qw/carp croak verbose/;
 
@@ -177,7 +177,7 @@ Pawel Krol, E<lt>pawelkrol@cpan.orgE<gt>.
 
 =head1 VERSION
 
-Version 0.01 (2010-10-10)
+Version 0.02 (2013-01-12)
 
 =head1 COPYRIGHT AND LICENSE
 
